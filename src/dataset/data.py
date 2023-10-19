6b3a1776fd2fa39bf9511edd7f6dcb1c28133413
JiaYanhao <2474840061@qq.com>
2023-10-19 10:50:12 +0800

    0.0.4

--- src/dataset/data.py
+++ src/dataset/data.py
@@ -6,7 +6,7 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 
 
-class StyleDataset(Dataset):
+class StyleT2IDataset(Dataset):
     def __init__(self, root_path, json_path, image_transform, tokenizer):
         self.root_path = root_path
         self.dataset = json.load(open(json_path,'r'))
@@ -20,7 +20,10 @@ class StyleDataset(Dataset):
     def __getitem__(self, index):
         caption_path = os.path.join(self.root_path, 'text/'+self.dataset[index]['caption'])
         image_path = os.path.join(self.root_path, 'images/'+self.dataset[index]['image'])
-        negative_path = os.path.join(self.root_path, 'images/'+self.dataset[np.random.randint(1, len(self.dataset))]['image'])
+        neg = np.random.randint(1, len(self.dataset))
+        while neg == index:
+            neg = np.random.randint(1, len(self.dataset))
+        negative_path = os.path.join(self.root_path, 'images/'+self.dataset[neg]['image'])
         
         f = open(caption_path, 'r')
         caption = f.readline().replace('\n', '')
@@ -28,3 +31,36 @@ class StyleDataset(Dataset):
         negative_image = self.image_transform(Image.open(negative_path))
 
         return [caption, pair_image, negative_image]
+    
+
+class StyleI2IDataset(Dataset):
+    def __init__(self, root_path, json_path, image_transform):
+        self.root_path = root_path
+        self.dataset = json.load(open(json_path,'r'))
+        self.image_transform = image_transform
+    
+
+    def __len__(self):
+        return len(self.dataset)
+    
+    
+    def __getitem__(self, index):
+        ori_path = os.path.join(self.root_path, 'images/'+self.dataset[index]['image'])
+        rand = np.random.randint(1,3)
+        if rand == 1:
+            pair_path = os.path.join(self.root_path, 'sketch/'+self.dataset[index]['image'])
+        elif rand == 2:
+            pair_path = os.path.join(self.root_path, 'art/'+self.dataset[index]['image'])
+        else:
+            pair_path = os.path.join(self.root_path, 'mosaic/'+self.dataset[index]['image'])
+
+        neg = np.random.randint(1, len(self.dataset))
+        while neg == index:
+            neg = np.random.randint(1, len(self.dataset))
+        negative_path = os.path.join(self.root_path, 'images/'+self.dataset[neg]['image'])
+        
+        ori_image = self.image_transform(Image.open(ori_path))
+        pair_image = self.image_transform(Image.open(pair_path))
+        negative_image = self.image_transform(Image.open(negative_path))
+
+        return [ori_image, pair_image, negative_image]
--- src/dataset/gen_colormap.py
+++ src/dataset/gen_colormap.py
@@ -2,14 +2,33 @@ import cv2
 import os
 import random
 import json
+from tqdm import tqdm
 
-def mosaic2(image, step=16):
+def mosaic2(image_path, out_path, step=16):
+    image = cv2.imread(image_path)
     h, w, _ = image.shape
     image2 = cv2.resize(image, (w // step, h // step))
     image3 = cv2.resize(image2, (w, h), interpolation=cv2.INTER_NEAREST)
-    cv2.imwrite('test.jpg', image3)
+    cv2.imwrite(out_path, image3)
 
 
 if __name__ == '__main__':
-	img = cv2.imread('imagenet/val/n01491361/ILSVRC2012_val_00002969.JPEG', 1)
-	temp = mosaic2(img)
+    # img = cv2.imread('imagenet/val/n01491361/ILSVRC2012_val_00002969.JPEG', 1)
+    # temp = mosaic2(img)
+    
+    root_path = 'fscoco/'
+    folderlist = os.listdir(root_path)
+    if os.path.exists(root_path + '/mosaic/'):
+        pass
+    else:
+        os.mkdir(root_path + '/mosaic/')
+
+    for folder in folderlist:
+        # os.mkdir(root_path + '/mosaic/{}'.format(folder))
+        filelist = os.listdir(root_path + '/images/{}'.format(folder))
+
+        for file in tqdm(filelist):
+            image_path = root_path + '/images/{0}/{1}'.format(folder, file)
+            mosaic_path = root_path + '/mosaic/{0}/{1}'.format(folder, file)
+
+            mosaic2(image_path, mosaic_path)
