b3f871740faf03c72e67162e343d4bbef27042ce
JiaYanhao <2474840061@qq.com>
2023-11-02 00:35:10 +0800

    0.2.4

---
+++ src/dataset/gen_cluster.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import json
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from sklearn.cluster import KMeans
+from open_clip.factory import image_transform
+
+
+root_path = ''
+
+image_mean = (0.48145466, 0.4578275, 0.40821073)
+image_std = (0.26861954, 0.26130258, 0.27577711)
+
+
+VGG = nn.Sequential(
+    nn.Conv2d(3, 3, (1, 1)),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(3, 64, (3, 3)),
+    nn.ReLU(),  # relu1-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 64, (3, 3)),
+    nn.ReLU(),  # relu1-2
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(64, 128, (3, 3)),
+    nn.ReLU(),  # relu2-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 128, (3, 3)),
+    nn.ReLU(),  # relu2-2
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(128, 256, (3, 3)),
+    nn.ReLU(),  # relu3-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 256, (3, 3)),
+    nn.ReLU(),  # relu3-4
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(256, 512, (3, 3)),
+    nn.ReLU(),  # relu4-1, this is the last layer used
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu4-4
+    nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-1
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-2
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU(),  # relu5-3
+    nn.ReflectionPad2d((1, 1, 1, 1)),
+    nn.Conv2d(512, 512, (3, 3)),
+    nn.ReLU()  # relu5-4
+)
+
+
+def get_features(image, model, layers=None):
+    if layers is None:
+        layers = {'0': 'conv1_1',  
+                  '5': 'conv2_1',  
+                  '10': 'conv3_1', 
+                  '19': 'conv4_1', 
+                  '21': 'conv4_2', 
+                  '28': 'conv5_1',
+                  '31': 'conv5_2'
+                 }  
+    features = {}
+    x = image
+    for name, layer in model._modules.items():
+        x = layer(x)   
+        if name in layers:
+            features[layers[name]] = x
+    
+    return features
+
+
+VGG.load_state_dict(torch.load('pretrained/vgg.pth'))
+
+filelist = json.load(open('train.json', 'r'))
+
+process = image_transform(224, False, image_mean, image_std)
+
+embed = []
+
+for file in tqdm(filelist):
+    path = root_path + 'images/' + file['images']
+    img = process(Image.open(path)).unsqueeze(0)
+    feat = get_features(img, VGG)
+    embed.append(feat['conv3_1'].detach().cpu())
+
+feature = torch.stack(embed, dim=0).detach().numpy().squeeze(1).view(len(filelist), -1)
+
+np.save('ori3_1.npy', feature)
+
+kmeans = KMeans(n_clusters=1)
+
+kmeans.fit(feature)
+
+cluster_labels = kmeans.labels_
+
+cluster_centers = kmeans.cluster_centers_
+
+cluster_tensor = np.mean(cluster_centers, axis=0)
+cluster_tensor = cluster_tensor.reshape((1, 1605632))
+
+np.save('origin_cluster.npy', cluster_tensor)
