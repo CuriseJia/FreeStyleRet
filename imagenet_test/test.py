6025cfb8950c7bb605e38e763c3a70a909bc011d
JiaYanhao <2474840061@qq.com>
2023-11-09 16:04:49 +0800

    0.2.9.2

--- imagenet_test/test.py
+++ imagenet_test/test.py
@@ -2,13 +2,12 @@ import argparse
 from tqdm import tqdm
 import sys
 import torch
-import open_clip
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from data import S2ITestDataset, T2ITestDataset, M2ITestDataset
-from comparison_test import Prompt_BLIP, Prompt_CLIP, Prompt_ImageBind
-from src.utils.utils import setup_seed, getR1Accuary, getR5Accuary
+from src.models import DeepStyleRetrieval
+from src.utils.utils import getR1Accuary, getR5Accuary
 
 
 def parse_args():
@@ -19,82 +18,51 @@ def parse_args():
     parser.add_argument('--device', default='cuda:0')
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--num_workers', default=6, type=int)
+    parser.add_argument('--gram_encoder_path', default='fscoco/vgg_normalised.pth', type=str, help='load vgg from given path')
+    parser.add_argument('--style_prompt_path', default='fscoco/style_cluster.npy', type=str, help='load vgg from given path')
 
     # data settings
     parser.add_argument("--type", type=str, default='style2image', help='choose train test2image or style2image.')
     parser.add_argument("--root_json_path", type=str, default='imagenet/test.json')
+    parser.add_argument("--other_json_path", type=str, default='imagenet/test.json')
     parser.add_argument("--root_file_path", type=str, default='imagenet/')
     parser.add_argument("--other_file_path", type=str, default='imagenet-s/')
     parser.add_argument("--batch_size", type=int, default=16)
 
     # model settings
-    parser.add_argument('--model', type=str, default='clip', help='Prompt_CLIP, Prompt_BLIP or Prompt_ImageBind.')
-    parser.add_argument('--n_prompts', type=int, default=3)
-    parser.add_argument('--prompt_dim', type=int, default=50176)
+    parser.add_argument('--gram_prompts', type=int, default=4)
+    parser.add_argument('--gram_prompt_dim', type=int, default=1024)
+    parser.add_argument('--style_prompts', type=int, default=4)
+    parser.add_argument('--style_prompt_dim', type=int, default=1024)
 
     args = parser.parse_args()
     return args
 
 
-def eval(args, model, dataloader, **kwargs):
+def S2IRetrieval(args, model, ori_images, pair_images):
 
-    r1 = []
-    r5 = []
+    ori_feat = model(ori_images, dtype='image')
+    ske_feat = model(pair_images, dtype='image')  
 
-    if args.type == 'text2image':
-        for data in enumerate(tqdm(dataloader)):
+    prob = torch.softmax(ske_feat.view(args.batch_size, -1) @ ori_feat.view(args.batch_size, -1).permute(1, 0), dim=-1)
 
-            caption = tokenizer(data[1][0]).to(device, non_blocking=True)
-            image = data[1][1].to(device, non_blocking=True)
+    return prob
 
-            image_feature = model.encode_image(image)
-            text_feature = model.encode_text(caption)
 
-            image_feature = F.normalize(image_feature, dim=-1)
-            text_feature = F.normalize(text_feature, dim=-1)
+def T2IRetrieval(args, model, ori_images, text_caption):
+    ori_feat = model(ori_images, dtype='image')
+    ske_feat = model(text_caption, dtype='text')
 
-            prob = torch.softmax((100.0 * text_feature @ image_feature.T), dim=-1)
+    prob = torch.softmax(ske_feat @ ori_feat.T, dim=-1)
 
-            r1.append(getR1Accuary(prob))
-            r5.append(getR5Accuary(prob))
-
-    else:
-        for data in enumerate(tqdm(dataloader)):
-            
-            origin_image = data[1][0].to(device, non_blocking=True)
-            retrival_image = data[1][1].to(device, non_blocking=True)
-
-            original_feature = model.encode_image(origin_image)
-            retrival_feature = model.encode_image(retrival_image)
-
-            original_feature = F.normalize(original_feature, dim=-1)
-            retrival_feature = F.normalize(retrival_feature, dim=-1)
-
-            prob = torch.softmax((100.0 * retrival_feature @ original_feature.T), dim=-1)
-
-            r1.append(getR1Accuary(prob))
-            r5.append(getR5Accuary(prob))
-
-    resr1 = sum(r1)/len(r1)
-    resr5 = sum(r5)/len(r5)
-    print(resr1)
-    print(resr5)
+    return prob
 
 
 if __name__ == "__main__":
     args = parse_args()
-    setup_seed(args.seed)
-    device = torch.device(args.device)
     
-    if args.model == 'clip':
-        model = Prompt_CLIP(args)
-        tokenizer = model.tokenizer
-    elif args.model == 'blip':
-        model = Prompt_BLIP(args)
-    else:
-        model = Prompt_ImageBind(args)
-
-    model.load_state_dict(torch.load(args.prompt_resume))
+    model = DeepStyleRetrieval(args)
+    model.load_state_dict(torch.load(args.resume))
     model.eval()
     model.to(args.device)
 
@@ -111,7 +79,32 @@ if __name__ == "__main__":
                             pin_memory=True,
                             prefetch_factor=16,
                             shuffle=False,
-                            drop_last=True
-                            )
+                            drop_last=True)
 
-    eval(args, model, test_loader, tokenizer)
+    r1 = []
+    r5 = []
+    
+    if args.type == 'text2image':
+        for data in enumerate(tqdm(test_loader)):
+            caption = model.tokenizer(data[1][1]).to(args.device, non_blocking=True)
+            image = data[1][0].to(args.device, non_blocking=True)
+
+            prob = T2IRetrieval(args, model, image, caption)
+
+            r1.append(getR1Accuary(prob))
+            r5.append(getR5Accuary(prob))
+
+    else:
+        for data in enumerate(tqdm(test_loader)):
+            origin_image = data[1][0].to(args.device, non_blocking=True)
+            retrival_image = data[1][1].to(args.device, non_blocking=True)
+
+            prob = S2IRetrieval(args, model, origin_image, retrival_image)
+
+            r1.append(getR1Accuary(prob))
+            r5.append(getR5Accuary(prob))
+
+    resr1 = sum(r1)/len(r1)
+    resr5 = sum(r5)/len(r5)
+    print(resr1)
+    print(resr5)
